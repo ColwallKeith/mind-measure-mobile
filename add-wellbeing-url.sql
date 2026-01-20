791fb702a1855c033c962cf2a7cd0e779701441c
Keith Duddy <keith@dicestudio.com>
2026-01-20 15:12:27 +0000

    Cleanup: Remove temporary SQL files

--- add-wellbeing-url.sql
+++
@@ -1,7 +0,0 @@
--- Add wellbeing_support_url to universities table
-ALTER TABLE universities ADD COLUMN IF NOT EXISTS wellbeing_support_url TEXT;
-
--- Set Worcester's wellbeing URL
-UPDATE universities 
-SET wellbeing_support_url = 'https://studentservices.on.worc.ac.uk/firstpoint/' 
-WHERE id = 'worcester';
